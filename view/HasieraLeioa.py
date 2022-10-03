81d9bd4d9f6980049aa3505f08f990631b2a0cc6
eneko <ebasauri002@ikasle.ehu.eus>
2022-10-03 17:03:00 +0200

    irudia gehitzea falta da

--- view/HasieraLeioa.py
+++ view/HasieraLeioa.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from view.ErregistroLeioa import ErregistroLeioa
 from view.SaioHasiLeioa import SaioHasiLeioa
 
-
 class HasieraLeioa(object):
 
     def __init__(self):
@@ -12,11 +11,18 @@ class HasieraLeioa(object):
         self.window.title("Tetris jokoa")
         self.window.config(bg="black")
 
-        buttonErregistro = tk.Button(self.window, text="ERREGISTRATU", command=self.clickEgin1)
-        buttonErregistro.pack()
-
         buttonSaioaHasi = tk.Button(self.window, text="SAIOA HASI", command=self.clickEgin2)
-        buttonSaioaHasi.pack()
+        buttonSaioaHasi.place(x=20, rely=0.6, width= 180)
+
+        buttonErregistro = tk.Button(self.window, text="ERREGISTRATU", command=self.clickEgin1)
+        buttonErregistro.place(x=20, rely=0.4,width=180)
+
+        egileak = tk.Label(text="Egileak:  Aimar Larrazabal",bg="black",fg="white")
+        egileak.place(relx=0.05, rely=0.85)
+        egileak2 = tk.Label(text="Eneko Perez", bg="black", fg="white")
+        egileak2.place(relx=0.25, rely=0.89)
+        egileak3 = tk.Label(text="Eneko Basauri", bg="black", fg="white")
+        egileak3.place(relx=0.25, rely=0.93)
 
         self.window.mainloop()
 
