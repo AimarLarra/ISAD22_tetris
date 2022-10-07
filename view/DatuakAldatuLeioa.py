462999582eab85a4b84dc6d96aa60a4193c0b07a
eneko <enekoperezbascones@gmail.com>
2022-10-07 19:03:06 +0200

    Erregistroa, saioa eta datuak aldatu amaituta

--- view/DatuakAldatuLeioa.py
+++ view/DatuakAldatuLeioa.py
@@ -48,25 +48,62 @@ class DatuakAldatuLeioa(object):
 
         def clickEgin1():
             if erab.get() and pasa.get() and erab2.get() and pasa2.get():
+                #Mirar si el nuevo usuario existe.
                 file = open("erabiltzaile_zerrenda", "r")
-                aurkitua = False
+                for k in file:
+                    e, f = k.split(",")
+                    if (e == erab2.get()):
+                        popupmsg("Erabiltzaile hori erregistratuta dago!")
+                        break
+                file.close()
+                file = open("erabiltzaile_zerrenda", "r")
+                file2 = open("erabiltzaile-zerrenda2", "w")
+                lehena=TRUE
+                ezdago=TRUE
                 for i in file:
                     a, b = i.split(",")
                     b = b.strip()
-                    if (a == erab.get() and b == pasa.get()):
-                        aurkitua = True
-                        #borrarlos
-                if aurkitua:
-                    file.close()
-                    file = open("erabiltzaile_zerrenda", "a")
-                    file.write("\n" + erab.get() + "," + pasa.get())
-                    file.close()
-                    self.window.destroy()
-                    ml.MenuLeioa()
+                    if (a != erab.get() or b != pasa.get()):
+                        if (lehena):
+                            file2.write( a + "," + b)
+                            lehena=FALSE
+                        else:
+                            file2.write("\n" +  a + "," + b)
+                    else:
+                        if(lehena):
+                            file2.write(erab2.get() + "," + pasa2.get())
+                            lehena=FALSE
+                            ezdago=FALSE
+                        else:
+                            file2.write("\n" + erab2.get() + "," + pasa2.get())
+                            ezdago=FALSE
+                file.close()
+                file2.close()
+                file = open("erabiltzaile_zerrenda", "w")
+                file2 = open("erabiltzaile-zerrenda2", "r")
+                lehena = TRUE
+                for j in file2:
+                    c, d = j.split(",")
+                    d = d.strip()
+                    if lehena:
+                        file.write(c + "," + d )
+                        lehena=FALSE
+                    else:
+                        file.write("\n" + c + "," + d )
+                file2.close()
+                file.close()
+                popupmsg("Erabiltzaile edo pasahitza txarto daude.")
             else:
                 popupmsg("Daturen bat hutsik dago!")
 
+        def clickEgin2():
+            self.window.destroy()
+            ml.MenuLeioa()
+
         buttonAldatu = tk.Button(self.window, text="ALDATU", command=clickEgin1)
         buttonAldatu.pack()
+
+        buttonItzuli= tk.Button(self.window, text="ITZULI", command=clickEgin2)
+        buttonItzuli.pack()
 
         self.window.mainloop()
