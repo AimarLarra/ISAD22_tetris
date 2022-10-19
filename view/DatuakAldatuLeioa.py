cdfa071f0f0cc17393021e78f5563c5bbc09e88d
Enekopb <91752156+Enekopb@users.noreply.github.com>
2022-10-19 23:39:10 +0200

    Update DatuakAldatuLeioa.py

--- view/DatuakAldatuLeioa.py
+++ view/DatuakAldatuLeioa.py
@@ -47,55 +47,19 @@ class DatuakAldatuLeioa(object):
             popup.mainloop()
 
         def clickEgin1():
-            if erab.get() and pasa.get() and erab2.get() and pasa2.get():
-                #Mirar si el nuevo usuario existe.
-                file = open("erabiltzaile_zerrenda", "r")
-                for k in file:
-                    e, f = k.split(",")
-                    if (e == erab2.get()):
-                        popupmsg("Erabiltzaile hori erregistratuta dago!")
-                        break
-                file.close()
-                file = open("erabiltzaile_zerrenda", "r")
-                file2 = open("erabiltzaile-zerrenda2", "w")
-                lehena=TRUE
-                ezdago=TRUE
-                for i in file:
-                    a, b = i.split(",")
-                    b = b.strip()
-                    if (a != erab.get() or b != pasa.get()):
-                        if (lehena):
-                            file2.write( a + "," + b)
-                            lehena=FALSE
-                        else:
-                            file2.write("\n" +  a + "," + b)
-                    else:
-                        if(lehena):
-                            file2.write(erab2.get() + "," + pasa2.get())
-                            lehena=FALSE
-                            ezdago=FALSE
-                        else:
-                            file2.write("\n" + erab2.get() + "," + pasa2.get())
-                            ezdago=FALSE
-                file.close()
-                file2.close()
-                file = open("erabiltzaile_zerrenda", "w")
-                file2 = open("erabiltzaile-zerrenda2", "r")
-                lehena = TRUE
-                for j in file2:
-                    c, d = j.split(",")
-                    d = d.strip()
-                    if lehena:
-                        file.write(c + "," + d )
-                        lehena=FALSE
-                    else:
-                        file.write("\n" + c + "," + d )
-                file2.close()
-                file.close()
-                popupmsg("Erabiltzaile edo pasahitza txarto daude.")
-            else:
-                popupmsg("Daturen bat hutsik dago!")
+            db = datuBasea()
+            if entry.get() and entry2.get() and entry3.get() and entry4.get():
+                emaitza = db.erabiltzailearenDatuakEguneratu(entry.get(), entry2.get(), entry3.get(), entry4.get())
+                if emaitza:
+                    # Si esta bien
+                    self.window.destroy()
+                    ml.MenuLeioa()
+                else:
+                    popupmsg("Erabiltzailea edo pasahitza oker daude!")
 
+            else:
+                popupmsg("Datuak ondo sartu!")
+                
         def clickEgin2():
             self.window.destroy()
             ml.MenuLeioa()
